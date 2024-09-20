4a226c2d795a98eecbec218fb1ff0e4408ac9355
andriyko31 <andrii.hladunenko@oa.edu.ua>
2024-09-20 11:51:09 +0300

    update lab 2

--- lab 2/src/App.jsx
+++ lab 2/src/App.jsx
@@ -1,13 +1,35 @@
 import React, { useState } from 'react'
 import PageTitle from './components/PageTitle'
-import ToDoContainer from './components/ToDoContainer'
+import AddToDoComponent from './components/AddToDoComponent'
+import SearchInput from './components/SearchInput'
+import ToDoTable from './components/ToDoTable'
+import './App.css'
 
 function ToDoApp() {
+  const [todos, setTodos] = useState([])
+  const [search, setSearch] = useState('')
+
+  const addTodo = (title) => {
+    const newTodoItem = { id: Date.now(), title }
+    setTodos([...todos, newTodoItem])
+  }
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
+  const filteredTodos = todos.filter((todo) =>
+    todo.title.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
     <div className="todo-app">
       <PageTitle title="ToDo App" />
-      <ToDoContainer />
+      <AddToDoComponent onAdd={addTodo} />
+      <SearchInput search={search} onSearchChange={setSearch} />
+      <ToDoTable todos={filteredTodos} onRemove={removeTodo} />
     </div>
   )
 }
+
 export default ToDoApp
